2d4275bcc5083300e2b0f478881dc0729d2d30e4
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-25 16:50:37 +0300

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,13 +19,11 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale():
     """get_locale function
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/", methods=["GET"])
