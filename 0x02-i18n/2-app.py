6b6d6d5d6fa405aa6cd4be6cc1cd4fb3d7fd6fe5
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-04-25 13:09:56 +0300

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,18 +18,20 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     """get_locale function
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home():
     """index function
     """
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,18 +19,20 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     """get_locale function
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route("/", methods=["GET"])
 def home():
     """index function
     """
-    return render_template("1-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
